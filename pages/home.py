346885fafe61f1c4b8cb18c3b334a267a2330258
1mouse111 <109869920+1mouse111@users.noreply.github.com>
2025-06-26 18:05:06 +0900

    comment markdown home.py

--- pages/home.py
+++ pages/home.py
@@ -42,12 +42,12 @@ conn.commit()
 
 # --- Streamlit UI Setup ---
 # ハンバーガーメニューの削除
-st.markdown("""
-    <style>
-    header {visibility: hidden;}
-    .css-18ni7ap.e8zbici2 {display: none;}
-    </style>
-""", unsafe_allow_html=True)
+# st.markdown("""
+#     <style>
+#     header {visibility: hidden;}
+#     .css-18ni7ap.e8zbici2 {display: none;}
+#     </style>
+# """, unsafe_allow_html=True)
 
 # トップバーの表示
 # topbar()
